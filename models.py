918318ee65d2a8305e705133b619bde6da796ac4
German Nunez <germannunez@Germans-MacBook-Pro.local>
2019-08-26 15:35:02 -0700

    heroku

--- models.py
+++ models.py
@@ -5,7 +5,9 @@ import os
 
 from playhouse.db_url import connect
 
-DATABASE = connect(os.environ.get('DATABASE_URL'))
+# DATABASE = connect(os.environ.get('DATABASE_URL'))
+
+DATABASE = SqliteDatabase('blogs.sqlite')
 
 class User(UserMixin, Model):
     username = CharField(unique=True)
